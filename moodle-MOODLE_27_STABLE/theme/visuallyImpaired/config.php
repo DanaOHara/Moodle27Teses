af04e867c7d3725c171c54b25db079ccb3f9dc26
DanaOHara <rosa.munozv@usach.cl>
2017-08-09 16:13:02 -0400

    Corraccion de archivo config.php

---
+++ moodle-MOODLE_27_STABLE/theme/visuallyImpaired/config.php
@@ -0,0 +1,89 @@
+#nombre del tema
+
+$THEME -> = 'visuallyImpaired';
+
+#nombre entre comillas de su clase padre
+$THEME->parents = array('base');
+
+#se indica donde se ubican nuestras hojas de estilos
+
+$THEME->sheets = array('visuallyImpaited');
+
+
+
+$THEME->layouts = array(
+    'base' => array(
+        'file' => 'standard.php',
+        'regions' => array(),
+    ),
+    'standard' => array(
+        'file' => 'standard.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    'course' => array(
+        'file' => 'standard.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post'
+    ),
+    'coursecategory' => array(
+        'file' => 'standard.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    'incourse' => array(
+        'file' => 'standard.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    'frontpage' => array(
+        'file' => 'standard.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    'admin' => array(
+        'file' => 'standard.php',
+        'regions' => array('side-pre'),
+        'defaultregion' => 'side-pre',
+    ),
+    'mydashboard' => array(
+        'file' => 'standard.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+        'options' => array('langmenu'=>true),
+    ),
+    'mypublic' => array(
+        'file' => 'standard.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-post',
+    ),
+    'login' => array(
+        'file' => 'standard.php',
+        'regions' => array(),
+        'options' => array('langmenu'=>true),
+    ),
+    'popup' => array(
+        'file' => 'standard.php',
+        'regions' => array(),
+        'options' => array('nofooter'=>true),
+    ),
+    'frametop' => array(
+        'file' => 'standard.php',
+        'regions' => array(),
+        'options' => array('nofooter'=>true),
+    ),
+    'maintenance' => array(
+        'file' => 'standard.php',
+        'regions' => array(),
+        'options' => array('nofooter'=>true, 'nonavbar'=>true),
+    ),
+    'print' => array(
+        'file' => 'standard.php',
+        'regions' => array(),
+        'options' => array('nofooter'=>true, 'nonavbar'=>false),
+    ),
+);
+
+/** List of javascript files that need to be included on each page */
+$THEME->javascripts = array();
+$THEME->javascripts_footer = array();
